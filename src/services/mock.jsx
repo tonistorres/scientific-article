51fdbf5f6294c2009d265a5920f5fe382f7e7783
tonistorres <sisvenda2011@gmail.com>
2022-09-02 00:21:26 -0300

    feat: difficulty in accessing api I made a mock to advance the project

---
+++ src/services/mock.jsx
@@ -0,0 +1,151 @@
+export const arr_mock = [
+    {
+        authors: "jão Pedro",
+        type: "romance",
+        title: "um amor para recordar",
+        description: 'xablau',
+        urls: [
+            {url:'http://citeseerx.ist.psu.edu/viewdoc/summary?doi=10.1.1.11.7696'},
+            {url:'http://citeseerx.ist.psu.edu/viewdoc/summary?doi=10.1.1.11.8129'}         ]
+    },
+    {
+        authors: "jão Pedro",
+        type: "romance",
+        title: "um amor para recordar",
+        description: 'xablau',
+        urls: [
+            {url:'http://citeseerx.ist.psu.edu/viewdoc/summary?doi=10.1.1.11.7696'},
+            {url:'http://citeseerx.ist.psu.edu/viewdoc/summary?doi=10.1.1.11.8129'} 
+        ]
+    },
+    {
+        authors: "jão Pedro",
+        type: "romance",
+        title: "um amor para recordar",
+        description: 'xablau',
+        urls: [
+            {url:'http://citeseerx.ist.psu.edu/viewdoc/summary?doi=10.1.1.11.7696'},
+            {url:'http://citeseerx.ist.psu.edu/viewdoc/summary?doi=10.1.1.11.8129'} 
+        ]
+    },
+    {
+        authors: "Biblia Sagrada Corrigida",
+        type: "Sagrado",
+        title: "Evangelho Mateus",
+        description: 'Esse é o evangelho segundo Matteh',
+        urls: [
+            {url:'http://citeseerx.ist.psu.edu/viewdoc/summary?doi=10.1.1.11.7696'},
+            {url:'http://citeseerx.ist.psu.edu/viewdoc/summary?doi=10.1.1.11.8129'} 
+        ]
+    },
+    {
+        authors: "Biblia Sagrada Corrigida e Atualizada",
+        type: "Sagrado",
+        title: "Evangelho Mateus",
+        description: 'Esse é o evangelho segundo Matteh',
+        urls: [
+            {url:'http://citeseerx.ist.psu.edu/viewdoc/summary?doi=10.1.1.11.7696'},
+            {url:'http://citeseerx.ist.psu.edu/viewdoc/summary?doi=10.1.1.11.8129'} 
+        ]
+    },
+    {
+        authors: "Biblia Sagrada Linguagem de Hoje",
+        type: "Sagrado",
+        title: "Evangelho Mateus",
+        description: 'Esse é o evangelho segundo Matteh',
+        urls: [
+            {url:'http://citeseerx.ist.psu.edu/viewdoc/summary?doi=10.1.1.11.7696'},
+            {url:'http://citeseerx.ist.psu.edu/viewdoc/summary?doi=10.1.1.11.8129'} 
+        ]
+    },
+    {
+        authors: "Biblia Sagrada Atualizada",
+        type: "Sagrado",
+        title: "Evangelho Mateus",
+        description: 'Esse é o evangelho segundo Matteh',
+        urls: [
+            {url:'http://citeseerx.ist.psu.edu/viewdoc/summary?doi=10.1.1.11.7696'},
+            {url:'http://citeseerx.ist.psu.edu/viewdoc/summary?doi=10.1.1.11.8129'}
+        ]
+    },
+    {
+        authors: "Biblia Sagrada Letra Gigante",
+        type: "Sagrado",
+        title: "Evangelho Mateus",
+        description: 'Esse é o evangelho segundo Matteh',
+        urls: [
+            {url:'http://citeseerx.ist.psu.edu/viewdoc/summary?doi=10.1.1.11.7696'},
+            {url:'http://citeseerx.ist.psu.edu/viewdoc/summary?doi=10.1.1.11.8129'}
+        ]
+    },
+    {
+        authors: "Biblia Sagrada Corrigida",
+        type: "Sagrado",
+        title: "Evangelho Mateus",
+        description: 'Esse é o evangelho segundo Matteh',
+        urls: [
+            {url:'http://citeseerx.ist.psu.edu/viewdoc/summary?doi=10.1.1.11.7696'},
+            {url:'http://citeseerx.ist.psu.edu/viewdoc/summary?doi=10.1.1.11.8129'}            
+        ]
+    },
+    {
+        authors: "Biblia Sagrada Corrigida",
+        type: "Sagrado",
+        title: "Evangelho Mateus",
+        description: 'Esse é o evangelho segundo Matteh',
+        urls: [
+            {url:'http://citeseerx.ist.psu.edu/viewdoc/summary?doi=10.1.1.11.7696'},
+            {url:'http://citeseerx.ist.psu.edu/viewdoc/summary?doi=10.1.1.11.8129'} 
+        ]
+    },
+    {
+        authors: "Biblia Sagrada Corrigida",
+        type: "Sagrado",
+        title: "Evangelho Mateus",
+        description: 'Esse é o evangelho segundo Matteh',
+        urls: [
+            {url:'http://citeseerx.ist.psu.edu/viewdoc/summary?doi=10.1.1.11.7696'},
+            {url:'http://citeseerx.ist.psu.edu/viewdoc/summary?doi=10.1.1.11.8129'} 
+        ]
+    },
+    {
+        authors: "Biblia Sagrada Corrigida",
+        type: "Sagrado",
+        title: "Evangelho Mateus",
+        description: 'Esse é o evangelho segundo Matteh',
+        urls: [
+            {url:'http://citeseerx.ist.psu.edu/viewdoc/summary?doi=10.1.1.11.7696'},
+            {url:'http://citeseerx.ist.psu.edu/viewdoc/summary?doi=10.1.1.11.8129'} 
+        ]
+    },
+    {
+        authors: "Biblia Sagrada Corrigida",
+        type: "Sagrado",
+        title: "Evangelho Mateus",
+        description: 'Esse é o evangelho segundo Matteh',
+        urls: [
+            {url:'http://citeseerx.ist.psu.edu/viewdoc/summary?doi=10.1.1.11.7696'},
+            {url:'http://citeseerx.ist.psu.edu/viewdoc/summary?doi=10.1.1.11.8129'} 
+        ]
+    },
+    {
+        authors: "Biblia Sagrada Corrigida",
+        type: "Sagrado",
+        title: "Evangelho Mateus",
+        description: 'Esse é o evangelho segundo Matteh',
+        urls: [
+            {url:'http://citeseerx.ist.psu.edu/viewdoc/summary?doi=10.1.1.11.7696'},
+            {url:'http://citeseerx.ist.psu.edu/viewdoc/summary?doi=10.1.1.11.8129'} 
+        ]
+    },
+    {
+        authors: "Biblia Sagrada Corrigida",
+        type: "Sagrado",
+        title: "Evangelho Mateus",
+        description: 'Esse é o evangelho segundo Matteh',
+        urls: [
+            {url:'http://citeseerx.ist.psu.edu/viewdoc/summary?doi=10.1.1.11.7696'},
+            {url:'http://citeseerx.ist.psu.edu/viewdoc/summary?doi=10.1.1.11.8129'} 
+        ]
+    },
+]
