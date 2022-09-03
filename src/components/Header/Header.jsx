3625c362ca150d58803422d938dc64bdcbe3f11a
tonistorres <sisvenda2011@gmail.com>
2022-09-02 23:18:07 -0300

    fix: implementing improvement Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import imgMettzer from '../../assets/mettzer.png'
 import '../../index.css';
 
-function Header() {
+function Header({ favoriteItems, dbFavorite, pageCurrent }) {
+
+    const navigate = useNavigate()
+
+    const handleClickFavorites = () => {
+        navigate('/favorite')
+    }
+
+    const handleClickHome = () => {
+        navigate('/')
+    }
+
     return (
         <div className="ct-main-header">
             <img src={imgMettzer} alt="logo Mettzer" className="logo-mettzer" />
-            <div className="ct-header-name">
-                <h1>Scientific Article</h1>
+            <h1> {`Scientific Article `}</h1>
+            <div>{dbFavorite.length > 0 ? <h1>⭐{favoriteItems}</h1> : null}</div>
+            <div>
+                {
+                    pageCurrent === 'Favorite'
+                        ?
+                        <button className="btn-ir-favorite" onClick={handleClickHome}>Home</button>
+                        :
+                        <button className="btn-ir-favorite" onClick={handleClickFavorites}>go Favorites</button>
+                }
             </div>
         </div>
     );
