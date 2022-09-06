15a2ba632b67019221e523d7ab7fa609e4704a0e
tonistorres <sisvenda2011@gmail.com>
2022-09-06 16:17:52 -0300

    fix: adding endpoint

--- src/services/Api.jsx
+++ src/services/Api.jsx
@@ -1,9 +1,15 @@
 import axios from "axios";
 
-export const apiCore = axios.create({
-  baseURL: "https://api.core.ac.uk/v3/search/works?apiKey=gStcq4C7GFURIwHQNryKTn9osuJ6DA5h",
-});
+// export const apiCoreEndPoint = axios.create({
+//   baseURL: "https://api.core.ac.uk/v3/search/works?apiKey=gStcq4C7GFURIwHQNryKTn9osuJ6DA5h",
+// });
+
+// export const apiCoreEndPoint =axios.create({
+//   baseURL: "https://core.ac.uk:443/api-v2/search/title:covid?page=3&pageSize=10&apiKey=gStcq4C7GFURIwHQNryKTn9osuJ6DA5h",
+// })
+
 
 export const apiCoreEndPoint =axios.create({
-  baseURL: "https://core.ac.uk:443/api-v2/search/%22search/data-providers%22?page=3&pageSize=10&apiKey=",
+  baseURL: "https://core.ac.uk:443/api-v2/search/title:psychology and language.name:English?page=3&pageSize=10&apiKey=gStcq4C7GFURIwHQNryKTn9osuJ6DA5h",
 })
+
