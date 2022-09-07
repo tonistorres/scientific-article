fa283754d8d7f694c2aa1aac02110c57004c4c08
tonistorres <sisvenda2011@gmail.com>
2022-09-07 18:01:14 -0300

    refactor: adding style

--- src/components/PaginationSelector/PaginationSelector.jsx
+++ src/components/PaginationSelector/PaginationSelector.jsx
@@ -5,7 +5,7 @@ function PaginationSelector({itensPerPage, setItensPerPage}) {
     return ( 
         <div className="ct-pagination-selector">
             
-        <select value={itensPerPage} onChange={(e) => setItensPerPage(Number(e.target.value))}>
+        <select value={itensPerPage} onChange={(e) => setItensPerPage(Number(e.target.value))} className="selct-pagination">
         <option value={2} key="">2</option>
             <option value={5} key="">5</option>
             <option value={10} key="">10</option>
