99748dfbf3fdf763c658c37bfcae50b083d4526e
tonistorres <sisvenda2011@gmail.com>
2022-09-07 16:19:04 -0300

    refactor: modifying file gif

--- src/components/Loading/Load.jsx
+++ src/components/Loading/Load.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import logo from '../../assets/loading.gif';
+import logo from '../../assets/boy.gif';
 import './Loading.css';
 
 
 function Load() {
     return (
         <div className="load-render">
-            <div><img src={logo} alt="Name: Mettzer" /></div>
+            <div><img src={logo} alt="Name: Mettzer" className="img-logo-load" /></div>
         </div>
     );
 }
