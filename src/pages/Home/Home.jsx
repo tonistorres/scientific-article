1e17178d9710816e657d1a06e969912f63757400
tonistorres <sisvenda2011@gmail.com>
2022-09-08 18:23:50 -0300

    refactor: componentizing component Table

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { FaStar } from 'react-icons/fa';
+
 import { getWorks, getTitle } from '../../services/Api';
 import { saveLocalStorage, searchLocalStorage } from '../../util/LocalStorage';
 import Pagination from "../../components/Pagination/Pagination";
 import Header from "../../components/Header/Header";
 import Load from "../../components/Loading/Load";
+import TableArticle from "../../components/Table/TableCustom";
+
 import '../../index.css';
 
 function Home() {
@@ -224,48 +226,8 @@ function Home() {
 
                     </div>
 
-                    <table className="table">
-                        <tr>
-                            <thead className="thead-light">
-                                <tr>
-                                    <th scope="col width: 10%"  >Authors</th>
-                                    <th scope="col width: 10%" >Type</th>
-                                    <th scope="col width: 10%" >Title</th>
-                                    <th scope="col width: 10%" >Description(s)</th>
-                                    <th scope="col width: 10%">url(s)</th>
-                                    <th scope="col width: 10%">Favorite</th>
-                                </tr>
-                            </thead>
-                            {dbAuthors.length > 0 && dbAuthors.map((item, index) => {
-                                return (
-                                    <tbody>
-                                        <tr key={index + 1} scope="row">
-                                            <td>{item._source.authors.map(item => <ul className="ul-none"><li>{item}</li></ul>)}</td>
-                                            <td>{item._type}</td>
-                                            <td>{item._source.title}</td>
-                                            <td>{item._source.description}</td>
-                                            <td>{
-                                                item._source.urls
-                                                    .map(item => <ul className="ul-none">
-                                                        <li><a href={item} target="_blank" rel="noreferrer" >{item}</a>
-                                                        </li>
-                                                    </ul>
-                                                    )
-                                            }
-                                            </td>
-                                            <td >
-                                                <div className="btn-favorite">
-                                                    <button className="btn-size-favorite" onClick={() => getId(item._id)}><FaStar size={30} /></button>
-                                                </div>
-                                            </td>
-                                        </tr>
-                                    </tbody>
-                                )
-                            })}
-                        </tr>
-                    </table>
+                    <TableArticle dbAuthors={dbAuthors} getId={getId}/>
 
-                    
                     <Pagination
                         controlePagina={controlePagina}
                         btnNext={btnNext}
