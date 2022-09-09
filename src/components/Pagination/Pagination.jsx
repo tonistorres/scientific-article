2114e334b5ed08326c6fc5bbe67b460a59247138
tonistorres <sisvenda2011@gmail.com>
2022-09-09 10:53:14 -0300

    refactor: component Pagination

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,21 +1,35 @@
 import React from "react";
 import './Pagination.css';
 
-function Pagination({ setCurrentPage, pages, btnNext,controlePagina,btnPrevious,valueSearchInput }) {
+function Pagination({ setCurrentPage, pages, btnNext, controlePagina, btnPrevious, valueSearchInput }) {
     return (
         <div className="ct-pagination">
-             <button onClick={(e)=>btnPrevious(valueSearchInput, controlePagina)}>⬅️</button>
-            {Array.from(Array(pages), (item, index) => {
-                return <button
-                    key={index}
-                    className="button-pagination"
-                    value={index}
-                    onClick={(e) => setCurrentPage(Number(e.target.value))}
-                >
-                   <span><strong>Pagina:</strong>{controlePagina}</span> 
+            <div className="ct-buttons">
+
+                <button
+                    onClick={(e) => btnPrevious(valueSearchInput, controlePagina)}
+                    className="btn-pagination">
+                    Previous
                 </button>
-            })}
-            <button onClick={(e)=>btnNext(valueSearchInput,controlePagina)}>➡️</button>
+
+                {Array.from(Array(pages), (item, index) => {
+                    return <div
+                        key={index}
+                        className="div-pagination"
+                        value={index}
+                        onClick={(e) => setCurrentPage(Number(e.target.value))}
+                    >
+                        <span>pg:{controlePagina}</span>
+                    </div>
+                })}
+
+                <button
+                    onClick={(e) => btnNext(valueSearchInput, controlePagina)}
+                    className="btn-next">
+                    Next
+                </button>
+
+            </div>
         </div>
 
     );
