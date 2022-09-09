ee0c424e5a00f3531e5d3deee99f32082ab65945
tonistorres <sisvenda2011@gmail.com>
2022-09-09 07:55:14 -0300

    feat: implementing TableCustom.jsx

--- src/components/Table/TableCustom.jsx
+++ src/components/Table/TableCustom.jsx
@@ -1,50 +1,53 @@
 import React from "react";
 import { FaStar } from 'react-icons/fa';
+import './Table.css';
 
 
-function TableArticle({dbAuthors,getId}) {
-    return (  
-        <table className="table">
-        <tr>
-            <thead className="thead-light">
-                <tr>
-                    <th scope="col width: 10%"  >Authors</th>
-                    <th scope="col width: 10%" >Type</th>
-                    <th scope="col width: 10%" >Title</th>
-                    <th scope="col width: 10%" >Description(s)</th>
-                    <th scope="col width: 10%">url(s)</th>
-                    <th scope="col width: 10%">Favorite</th>
-                </tr>
-            </thead>
-            {dbAuthors.length > 0 && dbAuthors.map((item, index) => {
-                return (
-                    <tbody>
-                        <tr key={index + 1} scope="row">
-                            <td>{item._source.authors.map(item => <ul className="ul-none"><li>{item}</li></ul>)}</td>
-                            <td>{item._type}</td>
-                            <td>{item._source.title}</td>
-                            <td>{item._source.description}</td>
-                            <td>{
-                                item._source.urls
-                                    .map(item => <ul className="ul-none">
-                                        <li><a href={item} target="_blank" rel="noreferrer" >{item}</a>
-                                        </li>
-                                    </ul>
-                                    )
-                            }
-                            </td>
-                            <td >
-                                <div className="btn-favorite">
-                                    <button className="btn-size-favorite" onClick={() => getId(item._id)}><FaStar size={30} /></button>
-                                </div>
-                            </td>
+function TableArticle({ dbAuthors, getId }) {
+    return (
+        <div className="table overflow-auto table-max-width">
+            <table className="rTable">
+            
+                <tbody>
+                <thead lassName="thead-light">
+                        <tr>
+                            <th scope="col">Authors</th>
+                            <th scope="col">Type</th>
+                            <th scope="col">Title</th>
+                            <th scope="col">Description(s)</th>
+                            <th scope="col">url(s)</th>
+                            <th scope="col">Favorite</th>
                         </tr>
-                    </tbody>
-                )
-            })}
-        </tr>
-    </table>
+                    </thead>
 
+                    {dbAuthors.length > 0 && dbAuthors.map((item, index) => {
+                        return (
+                            <tr key={item._id} scope="row">
+                                <td className="td-mw-authors" >{item._source.authors.map(item => <ul className="ul-none"><li>{item}</li></ul>)}</td>
+                                <td className="td-mw-type">{item._type}</td>
+                                <td className="td-mw-title">{item._source.title}</td>
+                                <td className="td-mw-descrip">{`${item._source.description}`.substring(0,50)+'...'}</td>
+                                <td className="td-mw-url">{
+                                    item._source.urls
+                                        .map(item => <ul className="ul-none">
+                                            <li><a href={item} target="_blank" rel="noreferrer" >{`Link`}</a>
+                                            </li>
+                                        </ul>
+                                        )
+                                }
+                                </td>
+                                <td className="td-mw-button" >
+                                    <div className="btn-favorite">
+                                        <button className="btn-size-favorite" onClick={() => getId(item._id)}><FaStar size={30} /></button>
+                                    </div>
+                                </td>
+                            </tr>
+
+                        )
+                    })}
+                </tbody>
+            </table>
+        </div>
     );
 }
 
