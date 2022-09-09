ca25689574dae03330e60a72a0c97494e73e44bb
tonistorres <sisvenda2011@gmail.com>
2022-09-09 10:55:14 -0300

    fix: correting bugs

--- src/pages/Favorite/Pagination.jsx
+++ src/pages/Favorite/Pagination.jsx
@@ -1,21 +1,28 @@
 import React from "react";
-import '../../index.css';
+import './PaginationFavorite.css';
 
 function Pagination({ setCurrentPage, pages }) {
-    return ( 
-               <div className="ct-pagination">
-                {Array.from(Array(pages), (item, index) => {
-                    return <button
-                        key={index}
-                        className="button-pagination"
-                        value={index}
-                        onClick={(e) => setCurrentPage(Number(e.target.value))}
-                    >
-                        {index + 1}
-                    </button>
-                })}
+    return (
+        <div className="ct-pagination-favorite">
+            <div className="ct-butons">
+            {Array.from(Array(pages), (item, index) => {
+                return (
+                    // <div >
+                        <button
+                            key={index}
+                            className="btn-pagination"
+                            value={index}
+                            onClick={(e) => setCurrentPage(Number(e.target.value))}
+                        >
+                            <strong>{index + 1}</strong>
+
+                        </button>
+                    // </div>
+                )
+            })}
             </div>
-        );
+        </div>
+    );
 
 
 }
