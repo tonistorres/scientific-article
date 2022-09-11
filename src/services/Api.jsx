c1abff39664164157cf08abb0e39d1921badecff
tonistorres <sisvenda2011@gmail.com>
2022-09-10 23:53:20 -0300

    fix: correting file rules eslint e prettier

--- src/services/Api.jsx
+++ src/services/Api.jsx
@@ -1,15 +1,13 @@
-import axios from "axios";
+import axios from 'axios';
 
 const API = axios.create({ baseURL: 'https://core.ac.uk:443/api-v2/search' });
 
-export const getWorks = async (endpoint) => {
-  const data = await API.get(endpoint)
-    .then((response) => response.data.data);
-  return data;
+export const getWorks = async endpoint => {
+	const data = await API.get(endpoint).then(response => response.data.data);
+	return data;
 };
 
-export const getTitle = async (endpoint) => {
-  const data = await API.get(endpoint)
-    .then((response) => response.data.data);
-  return data;
-}
+export const getTitle = async endpoint => {
+	const data = await API.get(endpoint).then(response => response.data.data);
+	return data;
+};
