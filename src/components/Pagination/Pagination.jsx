1bb422927d4f2ee1ff24ef5fa51a2349bb150f01
tonistorres <sisvenda2011@gmail.com>
2022-09-10 23:52:40 -0300

    fix: correting file rules eslint e prettier

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,38 +1,56 @@
-import React from "react";
+import React from 'react';
 import './Pagination.css';
+import PropTypes from 'prop-types';
 
-function Pagination({ setCurrentPage, pages, btnNext, controlePagina, btnPrevious, valueSearchInput }) {
-    return (
-        <div className="ct-pagination">
-            <div className="ct-buttons">
+function Pagination({
+	setCurrentPage,
+	btnNext,
+	controlePagina,
+	btnPrevious,
+	valueSearchInput,
+}) {
+	return (
+		<div className='ct-pagination'>
+			<div className='ct-buttons'>
+				<button
+					// eslint-disable-next-line prettier/prettier, no-unused-vars
+          onClick={(e) => btnPrevious(valueSearchInput, controlePagina)}
+					className='btn-pagination'
+				>
+					Previous
+				</button>
 
-                <button
-                    onClick={(e) => btnPrevious(valueSearchInput, controlePagina)}
-                    className="btn-pagination">
-                    Previous
-                </button>
+				{Array.from(Array(), (item, index) => {
+					return (
+						<div
+							key={index}
+							className='div-pagination'
+							value={index}
+							// eslint-disable-next-line prettier/prettier
+              onClick={(e) => setCurrentPage(Number(e.target.value))}
+						>
+							<span>pg:{controlePagina}</span>
+						</div>
+					);
+				})}
 
-                {Array.from(Array(pages), (item, index) => {
-                    return <div
-                        key={index}
-                        className="div-pagination"
-                        value={index}
-                        onClick={(e) => setCurrentPage(Number(e.target.value))}
-                    >
-                        <span>pg:{controlePagina}</span>
-                    </div>
-                })}
-
-                <button
-                    onClick={(e) => btnNext(valueSearchInput, controlePagina)}
-                    className="btn-next">
-                    Next
-                </button>
-
-            </div>
-        </div>
-
-    );
+				<button
+					// eslint-disable-next-line prettier/prettier, no-unused-vars
+          onClick={(e) => btnNext(valueSearchInput, controlePagina)}
+					className='btn-next'
+				>
+					Next
+				</button>
+			</div>
+		</div>
+	);
 }
 
+Pagination.propTypes = {
+	setCurrentPage: PropTypes.number,
+	valueSearchInput: PropTypes.string,
+	btnPrevious: PropTypes.func,
+	btnNext: PropTypes.func,
+	controlePagina: PropTypes.number,
+};
 export default Pagination;
