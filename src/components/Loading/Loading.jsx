dcb4c6f34bb2e76c5342e06425430012a5d0bbd8
tonistorres <sisvenda2011@gmail.com>
2022-09-10 23:52:32 -0300

    fix: correting file rules eslint e prettier

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -1,44 +1,49 @@
-
-import React, { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './Loading.css';
 import boy from '../../assets/boy.gif';
 
 function Loading() {
-    const navigate = useNavigate()
-    const [removeLoading, setRemoveLoading] = useState(false);
+	const navigate = useNavigate();
+	const [removeLoading, setRemoveLoading] = useState(false);
 
-    const handleClickFavorites = () => {
-        navigate('/home');
-    }
+	const handleClickFavorites = () => {
+		navigate('/home');
+	};
 
-    useEffect(() => {
-        const handleStateRedirect = async () => {
-            try {
-                setTimeout(() => {
-                    return setRemoveLoading(true);
-                }, 3000);
+	useEffect(() => {
+		const handleStateRedirect = async () => {
+			try {
+				setTimeout(() => {
+					return setRemoveLoading(true);
+				}, 3000);
+			} catch (error) {
+				console.log('Erro useEffect handleStateRedirect: ', error);
+			}
+		};
+		handleStateRedirect();
+	}, []);
 
-            } catch (error) {
-                console.log("Erro useEffect handleStateRedirect: ", error);
-            }
-        }
-        handleStateRedirect();
-    }, []);
+	if (removeLoading) return handleClickFavorites();
 
-    
-    if (removeLoading) return handleClickFavorites();
-
-    return (
-        <div>
-            <div className="lodad-main">
-                <div className='ct-oroganization'>
-                    <div><img src={boy} alt="logo Mettzer Boy" className='img-boy' /></div>
-                    <div><h3>Mettzer</h3></div>
-                </div>
-            </div>
-        </div>
-    );
+	return (
+		<div>
+			<div className='lodad-main'>
+				<div className='ct-oroganization'>
+					<div>
+						<img
+							src={boy}
+							alt='logo Mettzer Boy'
+							className='img-boy'
+						/>
+					</div>
+					<div>
+						<h3>Mettzer</h3>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Loading;
