085fbff75536d5edca8f5e57e5cfd8fff6ec5bdb
tonistorres <sisvenda2011@gmail.com>
2022-09-10 23:52:25 -0300

    fix: correting file rules eslint e prettier

--- src/components/Loading/Load.jsx
+++ src/components/Loading/Load.jsx
@@ -1,14 +1,15 @@
-import React from "react";
+import React from 'react';
 import logo from '../../assets/boy.gif';
 import './Loading.css';
 
-
 function Load() {
-    return (
-        <div className="load-render">
-            <div><img src={logo} alt="Name: Mettzer" className="img-logo-load" /></div>
-        </div>
-    );
+	return (
+		<div className='load-render'>
+			<div>
+				<img src={logo} alt='Name: Mettzer' className='img-logo-load' />
+			</div>
+		</div>
+	);
 }
 
-export default Load; 
+export default Load;
