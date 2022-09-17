488d51afe00f748934e0ab457319afa043d49c82
tonistorres <sisvenda2011@gmail.com>
2022-09-16 23:00:16 -0300

    refactor: adding component Modal in Home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,6 +5,8 @@ import Pagination from '../../components/Pagination/Pagination';
 import Header from '../../components/Header/Header';
 import TableResponsive from '../../components/Table/TableResponsive';
 import ScreenSearch from './ScreenSearch';
+import Modal from '../../components/Modal/Modal';
+
 import './Home.css';
 
 function Home() {
@@ -14,6 +16,7 @@ function Home() {
 	const [inputControlSearch, setInputControl] = useState(false);
 	const [valueSearchInput, setValueSearchInput] = useState('');
 	const [controlePagina, setControlePagina] = useState(1);
+	const [modalValue, setModalValue] = useState(false);
 
 	useEffect(() => {
 		const handleApiCORE = async () => {
@@ -93,13 +96,14 @@ function Home() {
 			) {
 				alert('Digite algo na Pesquisa');
 			} else {
+				setModalValue(true);
 				const response = await getTitle(
 					// eslint-disable-next-line no-undef
 					`/title:${valueSearchInput}?page=${controlePagina}&pageSize=10&apiKey=${process.env.REACT_APP_API_KEY}`,
 				);
+				setModalValue(false);
 				if (response !== null) {
 					checKeyFavoriteExist();
-					console.log('Dentro da Função dbAuthors', dbAuthors);
 					if (dbAuthors === null) {
 						setAuthors([]);
 					} else {
@@ -208,53 +212,55 @@ function Home() {
 	};
 
 	return (
-		<div className='main-home'>
-			<div className='main-home-sub-header'>
-				<Header dbFavorite={dbFavorite} dbAuthors={dbAuthors} />
-				<div className='main-group-component-main'>
-					<div className='ct-group-searc'>
-						<div className='ct-input-text'>
-							<div className='ct-sub-input-text'>
-								<input
-									type='text'
-									name='valueSearchInput'
-									placeholder='type your search'
-									onChange={handleInput}
-								/>
+		<>
+			<div className='main-home'>
+				<div className='main-home-sub-header'>
+					<Header dbFavorite={dbFavorite} dbAuthors={dbAuthors} />
+					<div className='main-group-component-main'>
+						<div className='ct-group-searc'>
+							<div className='ct-input-text'>
+								<div className='ct-sub-input-text'>
+									<input
+										type='text'
+										name='valueSearchInput'
+										placeholder='type your search'
+										onChange={handleInput}
+									/>
+								</div>
+							</div>
+							<div className='ct-select-button'>
+								<select
+									className='select-style'
+									onFocus={handleOptions}
+								>
+									<option value='title' selected>
+										Title
+									</option>
+								</select>
+								<button
+									className='btn-go-search'
+									// eslint-disable-next-line prettier/prettier, no-unused-vars
+                  onClick={(e) => searchTitle(valueSearchInput, controlePagina)}>
+									Go
+								</button>
 							</div>
 						</div>
-						<div className='ct-select-button'>
-							<select
-								className='select-style'
-								onFocus={handleOptions}
-							>
-								<option value='title' selected>
-									Title
-								</option>
-							</select>
-							<button
-								className='btn-go-search'
-								// eslint-disable-next-line prettier/prettier, no-unused-vars
-                  onClick={(e) => searchTitle(valueSearchInput, controlePagina)}>
-								Go
-							</button>
-						</div>
 					</div>
+					{dbAuthors.length === 0 ? (
+						<ScreenSearch />
+					) : (
+						<TableResponsive dbAuthors={dbAuthors} getId={getId} />
+					)}
+					<Modal valueClass={modalValue} />
+					<Pagination
+						controlePagina={controlePagina}
+						btnNext={btnNext}
+						btnPrevious={btnPrevious}
+						valueSearchInput={valueSearchInput}
+					/>
 				</div>
-				{dbAuthors.length === 0 ? (
-					<ScreenSearch />
-				) : (
-					<TableResponsive dbAuthors={dbAuthors} getId={getId} />
-				)}
-
-				<Pagination
-					controlePagina={controlePagina}
-					btnNext={btnNext}
-					btnPrevious={btnPrevious}
-					valueSearchInput={valueSearchInput}
-				/>
 			</div>
-		</div>
+		</>
 	);
 }
 
