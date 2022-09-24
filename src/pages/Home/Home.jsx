43c3d74396929e5902663706d552e5c8466678c4
tonistorres <sisvenda2011@gmail.com>
2022-09-24 11:29:50 -0300

    refactor: componetizing Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect, useContext } from 'react';
-import Pagination from '../../components/Pagination/Pagination';
+import ContextHome from '../../components/ContexMetzzer/ContextHome';
 import Header from '../../components/Header/Header';
 import TableResponsive from '../../components/Table/TableResponsive';
 import ScreenSearch from './ScreenSearch';
 import Modal from '../../components/Modal/Modal';
-import ContextHome from '../../components/ContexMetzzer/ContextHome';
+import Pagination from '../../components/Pagination/Pagination';
+import { SearchInput } from './SearchInput';
+import '../../index.css';
 import './Home.css';
 
 function Home() {
@@ -35,58 +37,28 @@ function Home() {
 	}, []);
 
 	return (
-		<div className='main-home'>
-			<div className='main-home-sub-header'>
-				<Header dbFavorite={dbFavorite} dbAuthors={dbAuthors} />
-				<div className='main-group-component-main'>
-					<div className='ct-group-searc'>
-						<div className='ct-input-text'>
-							<div className='ct-sub-input-text'>
-								<input
-									type='text'
-									name='valueSearchInput'
-									placeholder='type your search'
-									onChange={handleInput}
-								/>
-							</div>
-						</div>
-						<div className='ct-select-button'>
-							<select
-								className='select-style'
-								onFocus={handleOptions}
-							>
-								<option value='title' selected>
-									Title
-								</option>
-							</select>
-							<button
-								className='btn-go-search'
-								// eslint-disable-next-line prettier/prettier, no-unused-vars
-									onClick={e =>
-									searchTitle(
-										valueSearchInput,
-										controlePagina,
-									)
-								}
-							>
-								Go
-							</button>
-						</div>
-					</div>
-				</div>
-				{dbAuthors.length === 0 ? (
-					<ScreenSearch />
-				) : (
-					<TableResponsive dbAuthors={dbAuthors} getId={getId} />
-				)}
-				<Modal valueClass={modalValue} />
-				<Pagination
-					controlePagina={controlePagina}
-					btnNext={btnNext}
-					btnPrevious={btnPrevious}
-					valueSearchInput={valueSearchInput}
-				/>
-			</div>
+		<div className='ct-main-home'>
+			<Header dbFavorite={dbFavorite} dbAuthors={dbAuthors} />
+			<SearchInput
+				handleInput={handleInput}
+				handleOptions={handleOptions}
+				searchTitle={searchTitle}
+				valueSearchInput={valueSearchInput}
+				controlePagina={controlePagina}
+			/>
+
+			{/* {dbAuthors.length === 0 ? (
+				<ScreenSearch />
+			) : (
+				<TableResponsive dbAuthors={dbAuthors} getId={getId} />
+			)}
+			<Modal valueClass={modalValue} /> */}
+			{/* <Pagination
+				controlePagina={controlePagina}
+				btnNext={btnNext}
+				btnPrevious={btnPrevious}
+				valueSearchInput={valueSearchInput}
+			/> */}
 		</div>
 	);
 }
