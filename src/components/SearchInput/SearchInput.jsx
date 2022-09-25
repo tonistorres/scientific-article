a6741477417dd0461deac6b784f0d31f4fbec180
tonistorres <sisvenda2011@gmail.com>
2022-09-25 11:39:14 -0300

    feat: implementing file SearchInput

---
+++ src/components/SearchInput/SearchInput.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './SearchInput.css';
+import './SearchInputMediaHeight.css';
+import './SearchInputMediaWidth.css';
+
+export const SearchInput = ({
+	handleInput,
+	handleOptions,
+	searchTitle,
+	valueSearchInput,
+	controlePagina,
+}) => {
+	return (
+		<div className='main-group-component'>
+			<input
+				type='text'
+				name='valueSearchInput'
+				placeholder='type your search'
+				onChange={handleInput}
+			/>
+			<div className='ct-select-button'>
+				<select className='select-style' onFocus={handleOptions}>
+					<option value='title' selected>
+						Title
+					</option>
+				</select>
+				<button
+					className='btn-go-search'
+					onClick={e => searchTitle(valueSearchInput, controlePagina)}
+				>
+					Go
+				</button>
+			</div>
+		</div>
+	);
+};
+
+SearchInput.propTypes = {
+	valueSearchInput: PropTypes.string,
+	handleInput: PropTypes.func,
+	handleOptions: PropTypes.func,
+	searchTitle: PropTypes.func,
+	controlePagina: PropTypes.number,
+};
