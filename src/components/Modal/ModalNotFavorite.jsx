bfbdb69a27ee428dc17096cbd3e47126feb83b26
tonistorres <sisvenda2011@gmail.com>
2022-09-28 10:43:23 -0300

    refactor: adding functionalities in app

--- src/components/Modal/ModalNotFavorite.jsx
+++ src/components/Modal/ModalNotFavorite.jsx
@@ -4,7 +4,20 @@ import imgLoading from '../../assets/loading.gif';
 import './ModalNotFavorite.css';
 import PropTypes from 'prop-types';
 
-function ModalNotFavorite({ valueClass }) {
+function ModalNotFavorite({ valueClass, flagMsg }) {
+
+const modaListening = (str)=>{
+	switch(str) {
+
+		case "notfavorite":   return (<h1><p>There is not  </p><p>favorite item!!</p></h1>);
+		case "btnprevious":   return (<h1><p>We are at the</p><p> beginning of </p><p>the pagination!!</p></h1>);
+		case "btnnextsearch": return (<h1><p>Empty search field!!</p></h1>);
+		case "searchDigite":  return (<h1><p>type something </p><p>in the search</p></h1>);
+
+		default:      return <h1>No Component </h1>
+	  }
+
+}
 	return (
 		<div
 			id='modal-load'
@@ -13,7 +26,10 @@ function ModalNotFavorite({ valueClass }) {
 			}
 		>
 			<div className='modal-content'>
-				<h4><strong>Não existe Item Favoritado!!</strong></h4>
+				{
+					modaListening(flagMsg)
+				}
+
 			</div>
 		</div>
 	);
