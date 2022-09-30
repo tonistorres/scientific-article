31e0c8742626e7743ccbb66d92d0a242ef300091
tonistorres <sisvenda2011@gmail.com>
2022-09-30 16:03:28 -0300

    refactor: implementing app improvements

--- src/components/ScreenSearch/ScreenSearch.jsx
+++ src/components/ScreenSearch/ScreenSearch.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import logoBoy from '../../assets/boy.gif';
-import './ScreenSearch.css';
-import './ScreenSearchHeigth.css';
-import './ScreenMediaWidth.css';
+// import './ScreenSearch.css';
+// import './ScreenSearchHeigth.css';
+// import './ScreenMediaWidth.css';
+import './ScreenSearchIPhoneSE.css';
 
 function ScreenSearch() {
 	return (
