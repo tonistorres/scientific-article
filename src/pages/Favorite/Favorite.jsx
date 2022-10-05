98be609ee0c44b4a6e0c943429983dfe5fd999d6
tonistorres <sisvenda2011@gmail.com>
2022-10-05 13:03:07 -0300

    fix: correting bugs component Favorite

--- src/pages/Favorite/Favorite.jsx
+++ src/pages/Favorite/Favorite.jsx
@@ -1,21 +1,25 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { FaStar } from 'react-icons/fa';
-import { Navbar } from '../../components/Navbar/Navbar';
-import ContextFavorite from '../../components/ContexMetzzer/ContextFavorite';
 import TableFavorite from './TableFavorite/TableFavorite';
+import { Navbar } from '../../components/Navbar/Navbar';
+import { saveLocalStorage, searchLocalStorage } from '../../util/LocalStorage';
 import Pagination from './Pagination';
 import './Favorite.css';
 
 function Favorite() {
-	const {
-		initialState,
-		dbFavorite,
-		currentItens,
-		pageCurrent,
-		getId,
-		setCurrentPage,
-		pages,
-	} = useContext(ContextFavorite);
+	const navigate = useNavigate();
+	const SET_FAVORITE = 'Favorite';
+	const MAX_REGISTER_PER_PAGE = 10;
+	const [pageCurrent, setPageCurrent] = useState('Favorite');
+	const [dbFavorite, setDbFavorite] = useState([]);
+	const [itensPerPage, setItensPerPage] = useState(MAX_REGISTER_PER_PAGE);
+	const [currentPage, setCurrentPage] = useState(0);
+	const pages = Number(Math.ceil(dbFavorite.length / itensPerPage));
+	const startIndex = currentPage * itensPerPage;
+	const endIndex = startIndex + itensPerPage;
+	const currentItens = dbFavorite.slice(Number(startIndex), Number(endIndex));
+
 	useEffect(() => {
 		try {
 			initialState();
@@ -24,9 +28,66 @@ function Favorite() {
 		}
 	}, []);
 
+	const initialState = () => {
+		try {
+			setPageCurrent(SET_FAVORITE);
+			setItensPerPage(MAX_REGISTER_PER_PAGE);
+			const responseFavorite = searchLocalStorage('Favorite');
+			if (responseFavorite === null) {
+				saveLocalStorage('Favorite', []);
+				setDbFavorite([]);
+			} else {
+				setDbFavorite(responseFavorite);
+			}
+		} catch (error) {
+			console.log(`Erro function checkFavoriteExist:${error}`);
+		}
+	};
+
+	const getId = id => {
+		try {
+			const listFilter = dbFavorite.filter(item => item._id !== id);
+			setDbFavorite(listFilter);
+			saveLocalStorage('Favorite', [...listFilter]);
+			checkRedirect();
+		} catch (error) {
+			console.log(`Erro function getId:${error}`);
+		}
+	};
+
+	const handleClickHome = () => {
+		navigate('/home');
+	};
+
+	const checkRedirect = () => {
+		try {
+			const responseFavorite = searchLocalStorage('Favorite');
+			if (responseFavorite === null) {
+				handleClickHome();
+			} else if (responseFavorite.length === 0) {
+				handleClickHome();
+			}
+		} catch (error) {
+			console.log(`Erro function checkFavoriteExist:${error}`);
+		}
+	};
+
+	useEffect(() => {
+		try {
+			setCurrentPage(0);
+		} catch (error) {
+			console.log(`Erro useEffect itensPerPage:${error}`);
+		}
+	}, [itensPerPage]);
+
 	return (
 		<div className='main-favorite'>
-			<Navbar dbFavorite={dbFavorite} />
+			<Navbar
+				favoriteItems={dbFavorite.length}
+				dbAuthors={currentItens.length}
+				dbFavorite={dbFavorite}
+				pageCurrent={pageCurrent}
+			/>
 			<TableFavorite currentItens={currentItens} getId={getId} />
 			<Pagination setCurrentPage={setCurrentPage} pages={pages} />
 		</div>
