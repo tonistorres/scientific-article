151737046ec8428de8cb23c2959d9463ca1df024
tonistorres <sisvenda2011@gmail.com>
2022-10-06 16:28:10 -0300

    refactor: adding style in Component ScreenSearch

--- src/components/ScreenSearch/ScreenSearch.jsx
+++ src/components/ScreenSearch/ScreenSearch.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import logoBoy from '../../assets/boy.gif';
-// import './ScreenSearch.css';
-// import './ScreenSearchHeigth.css';
-// import './ScreenMediaWidth.css';
 import './ScreenSearchIPhoneSE.css';
+import './ScreenSearch.css';
 
 function ScreenSearch() {
 	return (
